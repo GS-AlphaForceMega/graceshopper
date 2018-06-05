301e491ed5511d9e8a7d9e047a9051d1cc51300e
Allen Roth <allenroth8888@gmail.com>
2018-06-05 17:37:46 -0400

    correctly rendering

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import ProductPreview from './productPreview';
 
 
 const products = [{id: 1, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 100, salePrice: 50, review: '****'},{id: 2, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 100, salePrice: 50, review: '****'},{id: 3, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 100, salePrice: 50, review: '****'},{id:4, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 100, salePrice: 50, review: '****'}]
@@ -9,13 +10,12 @@ const products = [{id: 1, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-c
 class AllProducts extends Component  {
 
     render(){
-        console.log("We are in allProductsssssss");
         return (
             <div>
                 <h1>We got it!!!!</h1>
                 <div>
                     {
-                        props.products.map(product => {
+                        this.props.products.map(product => {
                             return <Link to={`/products/${product.id}`}><ProductPreview product={product}/></Link>;
                         })
                     }
