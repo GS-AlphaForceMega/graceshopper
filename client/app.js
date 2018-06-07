e94e0f90f1619f7c72a85f12f9f9e60cf203fcfc
Graham Bass <grahambass@Geoffreys-MacBook-Pro.local>
2018-06-07 13:00:58 -0400

    reformatted component display

--- client/app.js
+++ client/app.js
@@ -1,19 +1,33 @@
 import React from 'react'
 
 import {Navbar} from './components'
-import Routes from './routes'
+// import Routes from './routes'
 import AllProducts from './components/allProducts.js'
 import Sidebar from './components/sidebar.js'
+import Cart from './components/Cart.js'
+import {Route, Redirect} from 'react-router-dom'
 
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       {/* <Routes /> */}
+//       <div className="middle-section">
+//         <Sidebar />
+//         <AllProducts />
+//       </div>
+//     </div>
+//   )
+// }
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <Routes /> */}
       <div className="middle-section">
         <Sidebar />
-        <AllProducts />
+        <Route exact path="/products" component={AllProducts} />
+        <Route exact path="/cart" component={Cart} />
       </div>
     </div>
   )
--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,26 +3,20 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import Item from './Item.jsx';
 
-
-const products = [{id: 1, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 100, salePrice: 50, review: '****', restaurant:{id: 1}},
+const items = [{id: 1, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 100, salePrice: 50, review: '****', restaurant:{id: 1}},
 {id: 2, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 110, salePrice: 50, review: '****', restaurant:{id: 1}},
 {id: 3, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 105, salePrice: 50, review: '****', restaurant:{id: 2}},
 {id:4, name: 'Rice', imageUrl: 'https://fgarciafoods.com/wp-content/uploads/2015/08/products-33.jpg', originalPrice: 120, salePrice: 50, review: '****', restaurant:{id: 2}}]
-const restaurantIds = [1, 2]
 
 class Cart extends Component  {
 
     render(){
-        console.log('restaurantIds in AllProducts', this.props.restaurantIds);
-        const restaurantIds = this.props.restaurantIds;
         return (
             <div>
                 <div className="all-items">
                     {
-                        this.props.products.map(product => {
-                            return restaurantIds.length >= 1 ? (this.props.restaurantIds.includes(product.restaurant.id) ? 
-                            <Link to={`/products/${product.id}`} key={product.id} ><Item product={product}/></Link>
-                            : null) : <Link to={`/products/${product.id}`} key={product.id} ><Item product={product}/></Link>;
+                        this.props.items.map(item => {
+                            <Link to={`/products/${item.id}`} key={item.id} ><Item item={item}/></Link>
                         })
                     }
                 </div>
@@ -35,11 +29,9 @@ class Cart extends Component  {
 const mapState = state => {
     return {
       isLoggedIn: !!state.user.id,
-      //change to state.products
-      products: products,
-      //change to state.restaurantIds
-      restaurantIds: state.restaurantIds
+      //change to state.items
+      items: items
     }
   } 
 
-export default connect(mapState)(AllProducts)
+  export default connect(mapState)(Cart)
