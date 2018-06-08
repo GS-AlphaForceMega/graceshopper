71ea9fbad6c959345b2fee6506830657e2008cb8
Allen Roth <allenroth8888@gmail.com>
2018-06-08 18:07:34 -0400

    car reducer ready to use

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,6 +4,7 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const ADD_TO_CART = 'ADD_TO_CART'
+const INCREASE_IN_CART = 'DECREASE_IN_CART'
 const DECREASE_IN_CART = 'DECREASE_IN_CART'
 const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 
@@ -16,6 +17,7 @@ const defaultCart = []
  * ACTION CREATORS
  */
 export const addToCart = (product, quantity) => ({type: ADD_CUISINE, product, quantity})
+export const increaseInCart = (productId) => ({type: DECREASE_IN_CART, productId})
 export const decreaseInCart = (productId) => ({type: DECREASE_IN_CART, productId})
 export const removeFromCart = (productId) => ({type: REMOVE_FROM_CART, productId})
 
@@ -33,12 +35,23 @@ export default function (state = defaultCart, action) {
   switch (action.type) {
     case ADD_TO_CART:
         return [...state, {product: action.product, quantity: action.quantity}]
+    case INCREASE_IN_CART:
+        return state.map(something => {
+            if (something.product.id === action.productId) {
+              something.quantity++;
+            }
+            return something;
+        })
     case DECREASE_IN_CART:
       return state.map(something => {
           if (something.product.id === action.productId) {
-            something.quantity--;
+            if (something.quantity > 1) {
+                something.quantity--;
+            } else {
+                return 
+            }
           }
-          return something;
+          return something
       })
     case REMOVE_FROM_CART:
         return state.filter(something => {
