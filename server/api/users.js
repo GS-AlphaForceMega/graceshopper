7d12884e73cf80a41a60ae0e868fee0831d674a8
Graham Bass <grahambass@Geoffreys-MacBook-Pro.local>
2018-06-08 12:50:15 -0400

    made user post and put routes more secure

--- server/api/users.js
+++ server/api/users.js
@@ -1,33 +1,55 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
   User.findAll()
-    .then(products => res.json(products))
+    .then(users => res.json(users))
     .catch(next);
 });
 
 router.get('/:id', (req, res, next) => {
   User.findById(req.params.id)
-    .then(product => res.json(product))
+    .then(user => res.json(user))
+    .catch(next);
+});
+
+router.get('/:id/orders', (req, res, next) => {
+  Order.findAll({
+    where: {
+      userId: req.params.id
+    }
+  })
+    .then(orders => res.json(orders))
     .catch(next);
 });
 
 router.post('/', (req, res, next) => {
-  User.create(req.body)
-    .then(product => res.send(product))
+  const { name, password } = req.body;
+  User.create({
+    name,
+    password
+  })
+    .then(user => res.send(user))
     .catch(next);
 });
 
 router.put('/:id', (req, res, next) => {
-  User.update(req.body, { where: { id: req.params.id } })
-    .then(product => res.json(product))
+  const { name, password } = req.body;
+  User.update({
+    name,
+    password
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(user => res.json(user))
     .catch(next);
 });
 
 router.delete('/:id', (req, res, next) => {
   User.destroy({ where: { id: req.params.id } })
-    .then(product => res.send(product))
+    .then(user => res.send(user))
     .catch(next);
 });
