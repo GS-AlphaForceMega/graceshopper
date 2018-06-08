70074aabf24d87ffbc824132b141927512480613
Allen Roth <allenroth8888@gmail.com>
2018-06-08 15:02:04 -0400

    minor fix

--- server/api/index.js
+++ server/api/index.js
@@ -5,8 +5,6 @@ module.exports = router;
 router.use('/users', require('./users'));
 router.use('/products', require('./products'));
 router.use('/restaurants', require('./restaurants'));
-// router.use('/cart', require('./cart'));
-// router.use('/orders', require('./orders'));
 router.use('/payment', require('./payment'));
 
 router.use((req, res, next) => {
