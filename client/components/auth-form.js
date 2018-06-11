51c24583f9817a1612ac70ace9dfc42fec4d7aaa
Mykola Maslov <kategorychnost@gmail.com>
2018-06-11 10:59:48 -0400

    monday morning for merge

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { auth } from '../store';
 import { Button, Form, Icon, Message } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
 
 /**
  * COMPONENT
@@ -12,57 +14,67 @@ const AuthForm = props => {
 
   return (
     <div className="auth-div">
-
       <Message
         attached
         header="Welcome to our site!"
         content={`Fill out the form below to ${displayName} for an account`}
       />
 
-      <Form className="auth-form attached fluid segment" onSubmit={handleSubmit} name={name} >
-      <div>
-        {name === 'signup' ? (
-          <Form.Group widths="equal">
-            <Form.Input
-              fluid
-              label="First Name"
-              placeholder="First Name"
-              type="text"
-              name="firstName"
-            />
-            <Form.Input
-              fluid
-              label="Last Name"
-              placeholder="Last Name"x
-              type="text"
-              name="lastName"
-            />
-          </Form.Group>
-        ) : (
-          <div />
-        )}
+      <Form
+        className="auth-form attached fluid segment"
+        onSubmit={handleSubmit}
+        name={name}
+      >
+        <div>
+          {name === 'signup' ? (
+            <Form.Group widths="equal">
+              <Form.Input
+                fluid
+                label="First Name"
+                placeholder="First Name"
+                type="text"
+                name="firstName"
+              />
+              <Form.Input
+                fluid
+                label="Last Name"
+                placeholder="Last Name"
+                type="text"
+                name="lastName"
+              />
+            </Form.Group>
+          ) : (
+            <div />
+          )}
         </div>
-        <Form.Input label="Email" placeholder="Email" type="email"  name="email" />
+        <Form.Input
+          label="Email"
+          placeholder="Email"
+          type="email"
+          name="email"
+        />
         <Form.Input label="Password" type="password" name="password" />
         <Form.Checkbox inline label="I agree to the terms and conditions" />
-        <Button color="blue" type="submit" >{displayName}</Button>
-        {error && error.response && <div> {error.response.data} </div>}
+        <Button color="blue" type="submit">
+          {displayName}
+        </Button>
+        {error && error.response && <div style={{color: "red"}} > {error.response.data} </div>}
       </Form>
 
-      <a href="/auth/google">{displayName} with Google</a>
+      <a href="/auth/google" >{displayName} with Google</a>
 
-{name === 'signup' ? (
-      <Message attached="bottom" warning>
-        <Icon name="help" />
-        Already signed up?&nbsp;<a href="/login">Login here</a>&nbsp;instead.
-      </Message>
-) : (
-  <Message attached="bottom" warning>
-        <Icon name="help" />
-        Don't have account?&nbsp;<a href="/signup">Sing up here</a>&nbsp;instead.
-      </Message>
-)}
-
+      {name === 'signup' ? (
+        <Message attached="bottom" warning>
+          <Icon name="help" />
+          Already signed up?&nbsp;<Link to="/login">Login here</Link>&nbsp;instead.
+        </Message>
+      ) : (
+        <Message attached="bottom" warning>
+          <Icon name="help" />
+          Don't have account?&nbsp;
+          <Link to="/signup">Sign Up here</Link>&nbsp;instead.
+        </Message>
+      )}
     </div>
   );
 };
