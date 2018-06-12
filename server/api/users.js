4cda41fc722bb97028960c0376fc5942daec797e
Andres Santos <andressantos94@gmail.com>
2018-06-12 14:47:47 -0400

    clean up done for server components

--- server/api/users.js
+++ server/api/users.js
@@ -85,21 +85,12 @@ router.get('/:id/orders/cart', (req, res, next) => {
 
 router.post('/:id/orders', (req, res, next) => {
   if (req.user && req.user.isAdmin === true || req.user && req.user.id === Number(req.params.id)) {
-<<<<<<< HEAD
-  const { orderId, productId /* quantity*/ } = req.body;
-=======
   const { orderId, productId, quantity } = req.body;
->>>>>>> ed2414a2c50dfa8c755feb7e04467d5d0fa7867b
   let findOrder = Order.findById(orderId);
   let findProduct = Product.findById(productId);
   return Promise.all([findOrder, findProduct])
   .then(([order, product]) => {
-<<<<<<< HEAD
-    //REWRITE THE QUANTITY
-    return order.addProduct(product, { through: { quantity: 1 }})
-=======
-    return order.addProduct(product,{ through: { quantity: quantity }})
->>>>>>> ed2414a2c50dfa8c755feb7e04467d5d0fa7867b
+    return order.addProduct(product, { through: { quantity: quantity }})
   })
   .then(() => {
     return Order.findById(orderId, {
