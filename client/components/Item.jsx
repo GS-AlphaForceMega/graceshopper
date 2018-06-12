ffc6dc802726c08fbf2488c215e75cd364148723
Graham Bass <grahambass@Geoffreys-MacBook-Pro.local>
2018-06-12 16:36:48 -0400

    stylized cart items

--- client/components/Item.jsx
+++ client/components/Item.jsx
@@ -11,22 +11,24 @@ const Item = (props) => {
     const orderId = props.order;
     const userId = Number(props.user.id);
     return (
-        <div className="item">
-            <Link to={`/products/${item.product.id}`}>
-                <div className="item_image">
+        <div className="item-div">
+            <Link to={`/products/${item.product.id}`} className="item-div-preview">
+                <div className="item-div-image">
                     <img className="preview-img" src={item.product.imageUrl} />
                 </div>
-                <div className="item_details">
-                    <div><h2>{item.product.name}</h2></div>
-                    <div><h2>Final Price: ${item.product.price}</h2></div>
-                    <div><h3>{item.product.description}</h3></div>
+                <div className="item-div-details">
+                    <div className="item-div-details"><h2>{item.product.name}</h2></div>
+                    <div className="item-div-details"><h3>{item.product.description}</h3></div>
+                    <div className="item-div-details"><h2>Final Price: ${item.product.price}</h2></div>
                 </div>
             </Link>
-            <h2>{item.quantity}</h2>
-            <div className="item-buttons">
-                <button onClick={() => props.increaseTheCart(userId, orderId, itemId)}>+</button>
-                <button onClick={() => props.decreaseTheCart(userId, orderId, itemId)}>-</button>
-                <button onClick={() => props.removeFromCart(userId, orderId, itemId)} >Remove</button>
+            <div className="item-div-quantity-and-buttons">
+                <div className="item-div-buttons">
+                    <button onClick={() => props.increaseTheCart(userId, orderId, itemId)}>+</button>
+                    <button onClick={() => props.decreaseTheCart(userId, orderId, itemId)}>-</button>
+                    <button onClick={() => props.removeFromCart(userId, orderId, itemId)} >Remove</button>
+                </div>
+                <h2>{item.quantity}</h2>
             </div>
         </div>
     )
